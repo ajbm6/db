71e74baa7ca0829791d10dab7735a76fc03fc094
Michael Bardsley <michael.bardsley@symphony-group.co.uk>
2013-08-12 14:56:41 +0100

    Fix minor issue, when no results were being found an exception was being thrown

--- src/Orno/Db/Driver/Oci8.php
+++ src/Orno/Db/Driver/Oci8.php
@@ -261,7 +261,8 @@ class Oci8 implements DriverInterface
             );
         }
 
-        return array_change_key_case(oci_fetch_assoc($this->statement));
+        $result = oci_fetch_assoc($this->statement);
+        return (empty($result)? false: array_change_key_case($result));
     }
 
     /**
@@ -278,7 +279,7 @@ class Oci8 implements DriverInterface
         }
 
         $result = oci_fetch_object($this->statement);
-        return (object) array_change_key_case((array) $result);
+        return (empty($result)? false: (object) array_change_key_case((array) $result));
     }
 
     /**
